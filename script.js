9a7aedff5fcbd1ac421640766b12153cfc8f1292
Eric Mok <ericmok@Erics-MacBook-Air.local>
2022-05-27 23:39:14 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,46 @@
+const storageInput = document.querySelector('.description');
+const saveButton = document.querySelector('.saveBtn');
+
+const saveToLocalStorage =()=>{
+    localStorage.setItem("input" , storageInput.value)
+}
+
+saveButton.addEventListener('click',saveToLocalStorage)
+
+function timeTracker() {
+
+    var timeNow = moment().hour();
+
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
+
+timeTracker();
